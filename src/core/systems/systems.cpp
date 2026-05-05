8c29bbb36ffcb3cb07b8fdd7bf37454608c0f5ff
adriank122 <40857582+adriank122@users.noreply.github.com>
2026-05-05 13:09:35 +0200

    Bugfix: Fix collision when entities not in the same tile

--- src/core/systems/systems.cpp
+++ src/core/systems/systems.cpp
@@ -5,7 +5,11 @@ namespace core {
 
 void checkCollisions(Game &game) {
   for (const Ghost &ghost : game.ghosts) {
-    if (ghost.x() == game.pman.x() && ghost.y() == game.pman.y()) {
+    bool sameTile = ghost.x() == game.pman.x() && ghost.y() == game.pman.y();
+    bool crossed =
+        ghost.x() == game.pman.prev_x() && ghost.y() == game.pman.prev_y() &&
+        game.pman.x() == ghost.prev_x() && game.pman.y() == ghost.prev_y();
+    if (sameTile || crossed) {
       game.lives--;
       game.pman.setPosition(game.spawnX, game.spawnY);
       return;
